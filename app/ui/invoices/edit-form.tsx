5c5497e9d8f4ab5959a00342890e04a3b99c176d
rsolahaei <rsolahaei@gmail.com>
2025-04-16 11:17:24 +0330

    handle eslint errors

--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -9,7 +9,7 @@ import {
 } from "@heroicons/react/24/outline";
 import Link from "next/link";
 import { Button } from "@/app/ui/button";
-import { UpdateInvoice } from "./buttons";
+// import { UpdateInvoice } from "./buttons";
 import { updateInvoice, State } from "@/app/lib/action";
 import { useActionState } from "react";
 
@@ -22,7 +22,10 @@ export default function EditInvoiceForm({
 }) {
   const initialState: State = { message: null, errors: {} };
   const updateInvoiceWithId = updateInvoice.bind(null, invoice.id);
-  const [state, formAction] = useActionState(updateInvoiceWithId, initialState);
+  const [_state, formAction] = useActionState(
+    updateInvoiceWithId,
+    initialState
+  );
   return (
     <form action={formAction}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
